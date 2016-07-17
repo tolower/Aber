363e3e28a889d55c0279b414738d5c92024e0e07
Xinglu Pu <lower_scoring@hotmail.com>
2016-07-17 13:14:25 +0800

    update

--- Aber/Aber/RegisterPage.xaml.cs
+++ Aber/Aber/RegisterPage.xaml.cs
@@ -20,21 +20,11 @@ namespace Aber
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class LoginPage : Page
+    public sealed partial class RegisterPage : Page
     {
-        public LoginPage()
+        public RegisterPage()
         {
             this.InitializeComponent();
-        }
-
-        private void registerHyperLinkBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(RegisterPage),null);
-        }
-
-        private void getpwdHyperLinkBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(GetPasswordPage), null);
         }
     }
 }
